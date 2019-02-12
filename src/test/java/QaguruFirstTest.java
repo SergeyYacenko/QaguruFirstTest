585bf608b34c26d8b448afbc1dc973aff3e7b877
SergeyY <sergejs.jacenko.lv@gmail.com>
2019-02-13 01:37:49 +0200

    More

--- src/test/java/QaguruFirstTest.java
+++ src/test/java/QaguruFirstTest.java
@@ -1,6 +1,8 @@
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class QaguruFirstTest {
@@ -11,8 +13,15 @@ public class QaguruFirstTest {
         WebDriver driver = new ChromeDriver();
         driver.get("http://qaguru.lv/register");
         driver.manage().window().maximize();
+
         String qTitle = driver.getTitle(); //quaguru get title variable
         System.out.println(qTitle);
         Assertions.assertEquals("Test", qTitle);
+
+        WebElement qName = driver.findElement(By.xpath("//input[@id='firstName']"));
+        qName.sendKeys("Ivans");
+        WebElement qSurn = driver.findElement(By.xpath("//input[@id='lastName']"));
+        qSurn.sendKeys("Berzins");
+        System.out.println();
     }
 }
